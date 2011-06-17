1da21b7608c0d849d7a8f324092a06e0dcc0f188
Daniela Wellisz <daniela.wellisz@gmail.com>
2011-06-17 14:50:16 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Baconverse::Application.config.secret_token = '953d4f9e35c1bdcae57c3e835bf1e47e9b8ef729bb1eaf6c74ef72555bc433c898a1f86e91704ec47f00bfe94064bd20de078ece642e28110995c9dc4a2d2b4e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Baconverse::Application.config.session_store :cookie_store, :key => '_baconverse_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Baconverse::Application.config.session_store :active_record_store
